e2b0a88922de906d95cc62fc90477532e6f78e19
Henrik Tran <tranh@student.chalmers.se>
2018-02-02 12:02:04 +0100

    test4

--- exercise2/script.js
+++ exercise2/script.js
@@ -1,139 +1,48 @@
 // Global variables
 // These are used for D3 graphs axises
 loadedData = false;
-domainX = [50, 85];
-domainY = [0, 5];
 
-var d3Update = false;
+var dimpleChart = false;
+var otherDimple;
 
-function drawD3Plot() {
-    var width = 600;
-    var height = 300;
-    var xoffs = 40;
-    var yoffs = 40;
+function drawDimplePlotZWEI(data) {
+  var svg = dimple.newSvg("#d3Container", 590, 400);
+       data = dimple.filterData(data, "noc", ["USA", "URS", "GER", "GBR", "FRA", "ITA", "SWE", "HUN", "AUS", "GDR"])
+       var otherDimple = new dimple.chart(svg, data);
+       otherDimple.setBounds(60, 30, 505, 305);
+       var x = otherDimple.addCategoryAxis("x", "edition");
+       x.addOrderRule("Date");
+       otherDimple.addMeasureAxis("y", "total");
+       var lol = otherDimple.addSeries("noc", dimple.plot.area);
+       otherDimple.addLegend(60, 10, 500, 20, "right");
+       lol.lineWeight = 0;
+       otherDimple.assignColor("USA", "#a6cee3");
+       otherDimple.assignColor("URS", "#1f78b4");
+       otherDimple.assignColor("GER",  "#b2df8a");
+       otherDimple.assignColor("GBR", "#33a02c");
+       otherDimple.assignColor("FRA", "#fb9a99");
+       otherDimple.assignColor("ITA", "#e31a1c");
+       otherDimple.assignColor("SWE", "#fdbf6f");
+       otherDimple.assignColor("HUN", "#ff7f00");
+       otherDimple.assignColor("AUS", "#cab2d6");
+       otherDimple.assignColor("GDR", "#6a3d9a");
 
-    var svg = d3.select("#d3Container")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-
-        // NAME THE DIFFERENT AXISES
-      svg.append("text")
-        .attr("class", "x label")
-        .attr("text-anchor", "end")
-        .attr("x", width/2)
-        .attr("y", height - 10)
-        .text("Temperature");
-
-        // y-axis name
-        svg.append("text")
-            .attr("class", "y label")
-            .attr("text-anchor", "end")
-            .attr("y", 1)
-            .attr("x", -height/3)
-            .attr("dy", ".75em")
-            .attr("transform", "rotate(-90)")
-            .text("Erosio Incidents");
-
-
-    // Get actual width
-    width = svg.node().getBoundingClientRect().width;
-
-    var x = d3.scaleLinear().domain(domainX).range([xoffs, width - 20]);
-    var y = d3.scaleLinear().domain(domainY).range([height - yoffs, 20]);
-
-    var linedata = d3.line()
-        .x(function (d) { return x(d["Temperature F"]) })
-        .y(function (d) { return y(d["Erosion incidents"]) });
-
-    // Add axes first to show plots on top
-    var x_axis = svg.append("g")
-        .classed("x axis", true)
-        .attr("transform", function () { return "translate(0," + y.range()[0] + ")" });
-
-    var y_axis = svg.append("g")
-        .classed("y axis", true)
-        .attr("transform", function () { return "translate(" + x.range()[0] + ",0)" });
-
-    // Add line first to show markers on top
-    var line_path = svg.append("path")
-        .data([loadedData])
-        .classed("line", true)
-        .attr("fill", "none")
-        .style("opacity", .8)
-        .attr("stroke", "rgb(7, 36, 18)")
-        .attr("stroke-width", "1px");
-
-    // Initial setup done - actual binding to data is done in update()!
-
-    function update() {
-
-        // Sort the data to make the line work right
-        loadedData.sort(function (x1, x2) {
-            return d3.ascending(x1["Temperature F"], x2["Temperature F"]);
-        })
-
-        line_path.data([loadedData]);
-
-        x.domain(domainX);
-        y.domain(domainY);
-        x_axis.transition().call(d3.axisBottom(x));
-        y_axis.transition().call(d3.axisLeft(y));
-        line_path.transition().attr("d", linedata);
-
-        var markers = svg.selectAll(".marker")
-            .data(loadedData);
-
-        markers.exit().remove();
-
-        var new_markers = markers.enter()
-            .append("g")
-            .classed("marker", true);
-
-
-        // CHANGE THESE TO CHANGE DATA IN PLOT
-        function marker_translation(d) {
-            return "translate(" + x(d["Temperature F"]) + ", " + y(d["Erosion incidents"]) + ")";
-        }
-
-        new_markers.attr("transform", marker_translation);
-        // Set translation without transition on enter, with transition on update
-        markers.transition().attr("transform", marker_translation);
-
-        new_markers.append("circle")
-            .attr("r", 10)
-            .style("opacity", .9)
-            .attr("fill", "MediumSeaGreen")
-            .attr("stroke", "rgb(7, 36, 18)");
-    }
-
-    // Call update to make inital data binding
-    update();
-
-    // Return update function to make it available for other
-    // functions to call when appropriate
-    return update;
+       otherDimple.draw();
 }
 
-function d3RandomZoom() {
-    if (d3Update) {
-        domainX = [Math.random() * 70, 85];
-        domainY = [0, 5 + Math.random() * 10];
-        d3Update();
-    }
-}
 
-function d3ResetZoom() {
-    if (d3Update) {
-        domainX = [50, 85];
-        domainY = [0, 5];
-        d3Update();
-    }
-}
+
+
+
+
+
+
+
+
 
 var dimpleChart = false;
 var dimpleChartz;
+
 
 function drawDimplePlot(data) {
   var svg = dimple.newSvg("#dimpleContainer", 590, 400);
@@ -142,6 +51,16 @@ function drawDimplePlot(data) {
   dimpleChartz.addMeasureAxis("p", "Total");
   var innerRing = dimpleChartz.addSeries(["Golds", "Silvers", "Bronzes", "Country"], dimple.plot.pie);
 
+  dimpleChartz.assignColor("USA", "#a6cee3");
+  dimpleChartz.assignColor("SovietUnion", "#1f78b4");
+  dimpleChartz.assignColor("Germany",  "#b2df8a");
+  dimpleChartz.assignColor("GreatBritain", "#33a02c");
+  dimpleChartz.assignColor("France", "#fb9a99");
+  dimpleChartz.assignColor("Italy", "#e31a1c");
+  dimpleChartz.assignColor("Sweden", "#fdbf6f");
+  dimpleChartz.assignColor("Hungary", "#ff7f00");
+  dimpleChartz.assignColor("Australia", "#cab2d6");
+  dimpleChartz.assignColor("EastGermany", "#6a3d9a");
   // Negatives are calculated from outside edge, positives from center
   innerRing.outerRadius = "0px";
   innerRing.innerRadius = "-50px";
@@ -161,14 +80,22 @@ function resetFilter(){
 }
 
 var damaged_csv = "https://dl.dropbox.com/s/8xcfjb8m2dvtnpn/olympicstats%20-%20Blad1%20%281%29.csv?dl=0";
+var data2_csv = "https://dl.dropbox.com/s/e0rxtxdsa2nhfui/medals.csv?dl=0";
 
 d3.csv(damaged_csv, function (data) {
     loadedData = data;
-
-    d3Update = drawD3Plot(data);
-
     drawDimplePlot(data);
 });
+
+d3.csv(data2_csv, function (data) {
+    loadedData = data;
+    drawDimplePlotZWEI(data);
+
+});
+
+function d3Filter() {
+    dimpleChart.data = dimple.filterData(loadedData,"total",["400"]);
+}
 
 function dimpleZoomReset() {
     dimpleChart.axes[0].overrideMin = 52;
